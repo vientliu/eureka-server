df4c472bf7d1a0b7df1094283c1825dd6da10634
tang <1220323287@qq.com>
2019-04-17 10:31:09 +0800

    注册中心  测试

--- eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EurekaServerApplication.class, args);
+		System.out.println("hello! world");
 	}
+
 
 }
